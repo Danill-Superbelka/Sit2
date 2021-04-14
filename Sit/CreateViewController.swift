5e8fc023f59cf52b4b63a9a9b70a7755e265c699
Daniil <51852490+Danill-Superbelka@users.noreply.github.com>
2021-04-14 11:25:17 +0300

    update CreateVC

--- Sit/CreateViewController.swift
+++ Sit/CreateViewController.swift
@@ -14,17 +14,21 @@ class CreateViewController: UIViewController {
     let organization: Results<Organization>
     let partitionValue: String
     var notificationToken: NotificationToken?
+    
     @IBOutlet var orgNameLabel: UITextField!
     @IBOutlet var typeOfData: UISegmentedControl!
     @IBOutlet var numberOfData: UISegmentedControl!
-
+    @IBOutlet var typeOfThreads: UISegmentedControl!
+    @IBOutlet var whoseData: UISegmentedControl!
+    @IBOutlet var addButton: UIButton!
+    
     required init?(coder: NSCoder,realm: Realm, title: String) {
         guard let syncConfiguration = realm.configuration.syncConfiguration else {
             fatalError("Sync configuratfgdion not found! Realm not opened with sync?");
         }
         self.realm = realm
         partitionValue = syncConfiguration.partitionValue!.stringValue!
-        organization = realm.objects(Organization.self).sorted(byKeyPath: "_id")
+        organization = realm.objects(Organization.self)
         super.init(coder: coder)
         self.title = title
     }
@@ -34,63 +38,41 @@ class CreateViewController: UIViewController {
     }
 
     deinit {
-        // Always invalidate any notification tokens when you are done with them.
         notificationToken?.invalidate()
     }
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        navigationItem.rightBarButtonItem = UIBarButtonItem(title: "Добавить", style: .done, target: self, action: #selector(addOrganization))
+        settings()
+    }
+    
+    @objc func settings(){
+//        navigationItem.rightBarButtonItem = UIBarButtonItem(title: "Добавить", style: .done, target: self, action: #selector(addOrganization))
+        addButton.layer.cornerRadius = 10
+        addButton.addTarget(self, action: #selector(addOrganization), for: .touchUpInside)
 
-        // Do any additional setup after loading the view.
     }
     
     @objc func addOrganization() {
         let classOfOrg: Int
-        let result = "\(typeOfData.selectedSegmentIndex + 1)\(numberOfData.selectedSegmentIndex + 1)"
+        let result = "\(typeOfData.selectedSegmentIndex + 1)\(whoseData.selectedSegmentIndex + 1)\(numberOfData.selectedSegmentIndex + 1)\(typeOfThreads.selectedSegmentIndex + 1)" // Тип данных, Данные сотрудников/внешних лиц, Объем данных, Типы угроз
+    
         switch result {
-        case "22":
+        case "1221","1211","1111","1121","2111","2211","1222","4221","4211","4111","4121":
+            classOfOrg = 1
+        case "3221","3211","3111","3121","1212","1112","1122","2112","2122","2212","2222","3222","4222","1223":
             classOfOrg = 2
-        case "23":
+        case "3212","3112","3122","4212","4112","4122","1213","1113","1123","2113","2123","2213","2223","4223":
             classOfOrg = 3
-        case "31":
-            classOfOrg = 2
-        case "32":
-            classOfOrg = 3
-        case "33":
-            classOfOrg = 3
-        case "41":
-            classOfOrg = 4
-        case "42":
-            classOfOrg = 4
-        case "43":
+        case "3223","3213","3113","3123","4213","4113","4123":
             classOfOrg = 4
         default:
-            classOfOrg = 1
+            classOfOrg = 0
         }
-        let org = Organization(partition: self.partitionValue, name: orgNameLabel.text!, protectionClass: String(classOfOrg), percentage: String("0%"))
+        
+        let org = Organization(partition: self.partitionValue, name: orgNameLabel.text!, protectionClass: String(classOfOrg), percentage: String("0"))
         try! self.realm.write{
             self.realm.add(org)
         }
     }
-    
-    
-    
-    
-    
-    
-    
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
